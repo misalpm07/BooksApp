1ab048d4158ac4650b102b2463c3e98b2afe8351
misalpm07 <63534788+misalpm07@users.noreply.github.com>
2020-05-04 21:37:33 -0700

    contactus

--- app/src/main/java/com/example/booksapp/bookrequestactivity.java
+++ app/src/main/java/com/example/booksapp/bookrequestactivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class contactus extends AppCompatActivity {
+public class bookrequestactivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contactus);
+        setContentView(R.layout.activity_bookrequestactivity);
     }
-
 }
