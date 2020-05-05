016dfdb8b7635c091aade3a94a2764b3230330de
misalpm07 <63534788+misalpm07@users.noreply.github.com>
2020-05-04 17:43:11 -0700

    contactuspage

--- app/src/main/java/com/example/booksapp/contactus.java
+++ app/src/main/java/com/example/booksapp/contactus.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class  loginactivity extends AppCompatActivity {
+public class contactus extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loginactivity);
+        setContentView(R.layout.notification_action);
     }
 }
